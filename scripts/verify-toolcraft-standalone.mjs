5d6fefe370da06347d96b47adf37c8cc53edc460
Kamil Jopek <kjopek@quora.com>
2026-07-30 16:21:12 +0000

    test(toolcraft-openapi): reflect public auth store import

--- scripts/verify-toolcraft-standalone.mjs
+++ scripts/verify-toolcraft-standalone.mjs
@@ -381,8 +381,7 @@ try {
       requiredEntries: [
         "package/dist/index.js",
         "package/dist/bin/generate.js",
-        "package/node_modules/toolcraft-design/package.json",
-        "package/node_modules/auth-store/package.json"
+        "package/node_modules/toolcraft-design/package.json"
       ]
     }
   ];
